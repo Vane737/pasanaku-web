9d19f5fe691159272c5e61a20388f91adbbf2a38
Vane737 <77525733+Vane737@users.noreply.github.com>
2024-03-24 22:56:04 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='text-primary'>Hola mundo con tailwind</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+     extend: {
+      colors: {
+        primary: '#F39C12',
+        secondary: '#DD4B39',
+        dark: '#1E1E1E',
+      },
+     },
+   },
+   plugins: [],
+ };
